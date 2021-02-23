2fd9d7e16638bced408968058bd93189d7e4f178
Suresh Kumar <psureshkumar21@gmail.com>
2021-02-23 17:56:23 +0530

    changes are added

--- Example.java
+++ Example.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
 for(int i=0;i<=10;i++)
 {
 System.out.println("Hello This is Sample Java Program:");
+System.out.println(" Welcome to the world of Java");
 }
 }
 }
